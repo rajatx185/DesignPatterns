467a9fb78077e0b30465df469d6be1ff0fb9be63
Rajat Gupta <rajat.g@pharmeasy.in>
2022-11-01 16:05:57 +0530

    Basics of Java #1

---
+++ OOPSPatterns/src/com/java/learn/Main.java
@@ -0,0 +1,8 @@
+package com.java.learn;
+
+public class Main {
+    public static void main(String[] args) {
+        User user = new User("rajat g");
+        System.out.println(user.name);
+    }
+}
---
+++ OOPSPatterns/src/com/java/learn/User.java
@@ -0,0 +1,13 @@
+package com.java.learn;
+
+// public means that the class, attribute, method etc can be accessed anywhere in the project
+// static means that the method can be called with creating an instance of the class
+
+public class User {
+    public String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+
+}
