1d6ceac1d1b88797df5bd21a82663f1899e0d954
Rajat Gupta <rajat.g@pharmeasy.in>
2022-11-01 22:58:21 +0530

    Basics of Java #2

--- OOPSPatterns/src/com/java/learn/User.java
+++ OOPSPatterns/src/com/java/learn/User.java
@@ -4,10 +4,16 @@ package com.java.learn;
 // static means that the method can be called with creating an instance of the class
 
 public class User {
+    // Fields
     public String name;
 
+    // Constructor
     public User(String name) {
         this.name = name;
     }
 
+    // Methods
+    public void sayHello() {
+        System.out.println("Hi, my name is " + name);
+    }
 }
